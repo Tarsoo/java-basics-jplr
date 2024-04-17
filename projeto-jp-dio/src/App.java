5417ee87f2075aefc84614e05a0109da27e524a2
Joao Pedro <jplr16@gmail.com>
2024-04-16 22:05:09 -0300

    grande aprendizado

--- projeto-jp-dio/src/App.java
+++ projeto-jp-dio/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("sou foda, dig din dig din dig din");
+        System.out.println("incrível");
+
+        int exit$_money = 1;
+        int exit$_money2 = 2;
+
+        int soma = somar(exit$_money, exit$_money2);
+        System.out.println(soma);
+    }
+
+    public static int somar(int exit$_money, int exit$_money2){
+        return exit$_money + exit$_money2;
     }
 }
